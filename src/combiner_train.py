6689a3cbadfbfca1b9f6bc48be4b6acc1a72ecb3
hoanghuydo787 <hoanghuydo787@gmail.com>
2024-04-08 11:17:29 +0700

    update

--- src/combiner_train.py
+++ src/combiner_train.py
@@ -227,6 +227,212 @@ def combiner_training_fiq(train_dress_types: List[str], val_dress_types: List[st
                     save_model(f'combiner_{epoch}', epoch, combiner, training_path)
 
 
+def combiner_training_adidas(train_dress_types: List[str], val_dress_types: List[str],
+                          projection_dim: int, hidden_dim: int, num_epochs: int, clip_model_name: str,
+                          combiner_lr: float, batch_size: int, clip_bs: int, validation_frequency: int,
+                          transform: str, save_training: bool, save_best: bool, **kwargs):
+    """
+    Train the Combiner on Adidas dataset keeping frozed the CLIP model
+    :param train_dress_types: Adidas categories to train on
+    :param val_dress_types: Adidas categories to validate on
+    :param projection_dim: Combiner projection dimension
+    :param hidden_dim: Combiner hidden dimension
+    :param num_epochs: number of epochs
+    :param clip_model_name: CLIP model you want to use: "RN50", "RN101", "RN50x4"...
+    :param combiner_lr: Combiner learning rate
+    :param batch_size: batch size of the Combiner training
+    :param clip_bs: batch size of the CLIP feature extraction
+    :param validation_frequency: validation frequency expressed in epoch
+    :param transform: preprocess transform you want to use. Should be in ['clip', 'squarepad', 'targetpad']. When
+                targetpad is also required to provide `target_ratio` kwarg.
+    :param save_training: when True save the weights of the Combiner network
+    :param save_best: when True save only the weights of the best Combiner wrt the average_recall metric
+    :param kwargs: if you use the `targetpad` transform you should prove `target_ratio` as kwarg. If you want to load a
+                fine-tuned version of clip you should provide `clip_model_path` as kwarg.
+    """
+
+    training_start = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    training_path: Path = Path(
+        base_path / f"models/combiner_trained_on_adidas_{clip_model_name}_{training_start}")
+    training_path.mkdir(exist_ok=False, parents=True)
+
+    # Save all the hyperparameters on a file
+    with open(training_path / "training_hyperparameters.json", 'w+') as file:
+        json.dump(training_hyper_params, file, sort_keys=True, indent=4)
+
+    clip_model, clip_preprocess = clip.load(clip_model_name, device=device, jit=False)
+
+    clip_model.eval()
+    input_dim = clip_model.visual.input_resolution
+    feature_dim = clip_model.visual.output_dim
+
+    if transform == "clip":
+        preprocess = clip_preprocess
+        print('CLIP default preprocess pipeline is used')
+    elif transform == "squarepad":
+        preprocess = squarepad_transform(input_dim)
+        print('Square pad preprocess pipeline is used')
+    elif transform == "targetpad":
+        target_ratio = kwargs['target_ratio']
+        preprocess = targetpad_transform(target_ratio, input_dim)
+        print(f'Target pad with {target_ratio = } preprocess pipeline is used')
+    else:
+        raise ValueError("Preprocess transform should be in ['clip', 'squarepad', 'targetpad']")
+
+    if kwargs.get("clip_model_path"):
+        print('Trying to load the CLIP model')
+        clip_model_path = kwargs["clip_model_path"]
+        saved_state_dict = torch.load(clip_model_path, map_location=device)
+        clip_model.load_state_dict(saved_state_dict["CLIP"])
+        print('CLIP model loaded successfully')
+
+    clip_model = clip_model.float()
+
+    idx_to_dress_mapping = {}
+    relative_val_datasets = []
+    index_features_list = []
+    index_names_list = []
+
+    # Define the validation datasets and extract the validation index features for each dress_type
+    for idx, dress_type in enumerate(val_dress_types):
+        idx_to_dress_mapping[idx] = dress_type
+        relative_val_dataset = FashionIQDataset('val', [dress_type], 'relative', preprocess)
+        relative_val_datasets.append(relative_val_dataset)
+        classic_val_dataset = FashionIQDataset('val', [dress_type], 'classic', preprocess)
+        index_features_and_names = extract_index_features(classic_val_dataset, clip_model)
+        index_features_list.append(index_features_and_names[0])
+        index_names_list.append(index_features_and_names[1])
+
+    # Define the combiner and the train dataset
+    combiner = Combiner(feature_dim, projection_dim, hidden_dim).to(device, non_blocking=True)
+    relative_train_dataset = FashionIQDataset('train', train_dress_types, 'relative', preprocess)
+    relative_train_loader = DataLoader(dataset=relative_train_dataset, batch_size=batch_size,
+                                       num_workers=multiprocessing.cpu_count(), pin_memory=True, collate_fn=collate_fn,
+                                       drop_last=True, shuffle=True)
+
+    # Define the optimizer, the loss and the grad scaler
+    optimizer = optim.Adam(combiner.parameters(), lr=combiner_lr)
+    crossentropy_criterion = nn.CrossEntropyLoss()
+    scaler = torch.cuda.amp.GradScaler()
+
+    # When save_best == True initialize the best result to zero
+    if save_best:
+        best_avg_recall = 0
+
+    # Define dataframes for CSV logging
+    training_log_frame = pd.DataFrame()
+    validation_log_frame = pd.DataFrame()
+
+    # Start with the training loop
+    print('Training loop started')
+    for epoch in range(num_epochs):
+        if torch.cuda.is_available():  # RuntimeError: "slow_conv2d_cpu" not implemented for 'Half'
+            clip.model.convert_weights(clip_model)  # Convert CLIP model in fp16 to reduce computation and memory
+        with experiment.train():
+            train_running_results = {'images_in_epoch': 0, 'accumulated_train_loss': 0}
+            combiner.train()
+            train_bar = tqdm(relative_train_loader, ncols=150)
+            for idx, (reference_images, target_images, captions) in enumerate(train_bar):  # Load a batch of triplets
+                step = len(train_bar) * epoch + idx
+                images_in_batch = reference_images.size(0)
+
+                optimizer.zero_grad()
+
+                reference_images = reference_images.to(device, non_blocking=True)
+                target_images = target_images.to(device, non_blocking=True)
+
+                # Randomize the training caption in four way: (a) cap1 and cap2 (b) cap2 and cap1 (c) cap1 (d) cap2
+                flattened_captions: list = np.array(captions).T.flatten().tolist()
+                input_captions = generate_randomized_fiq_caption(flattened_captions)
+                text_inputs = clip.tokenize(input_captions, truncate=True).to(device, non_blocking=True)
+
+                # Extract the features with CLIP
+                with torch.no_grad():
+                    reference_images_list = torch.split(reference_images, clip_bs)
+                    reference_image_features = torch.vstack(
+                        [clip_model.encode_image(mini_batch).float() for mini_batch in reference_images_list])
+                    target_images_list = torch.split(target_images, clip_bs)
+                    target_image_features = torch.vstack(
+                        [clip_model.encode_image(mini_batch).float() for mini_batch in target_images_list])
+
+                    text_inputs_list = torch.split(text_inputs, clip_bs)
+                    text_features = torch.vstack(
+                        [clip_model.encode_text(mini_batch).float() for mini_batch in text_inputs_list])
+
+                # Compute the logits and the loss
+                with torch.cuda.amp.autocast():
+                    logits = combiner(reference_image_features, text_features, target_image_features)
+                    ground_truth = torch.arange(images_in_batch, dtype=torch.long, device=device)
+                    loss = crossentropy_criterion(logits, ground_truth)
+
+                # Backprogate and update the weights
+                scaler.scale(loss).backward()
+                scaler.step(optimizer)
+                scaler.update()
+
+                experiment.log_metric('step_loss', loss.detach().cpu().item(), step=step)
+                update_train_running_results(train_running_results, loss, images_in_batch)
+                set_train_bar_description(train_bar, epoch, num_epochs, train_running_results)
+
+            train_epoch_loss = float(
+                train_running_results['accumulated_train_loss'] / train_running_results['images_in_epoch'])
+            experiment.log_metric('epoch_loss', train_epoch_loss, epoch=epoch)
+
+            # Training CSV logging
+            training_log_frame = pd.concat(
+                [training_log_frame,
+                 pd.DataFrame(data={'epoch': epoch, 'train_epoch_loss': train_epoch_loss}, index=[0])])
+            training_log_frame.to_csv(str(training_path / 'train_metrics.csv'), index=False)
+
+        if epoch % validation_frequency == 0:
+            clip_model = clip_model.float()  # In validation we use fp32 CLIP model
+            with experiment.validate():
+                combiner.eval()
+                recalls_at10 = []
+                recalls_at50 = []
+
+                # Compute and log validation metrics for each validation dataset (which corresponds to a different
+                # FashionIQ category)
+                for relative_val_dataset, index_features, index_names, idx in zip(relative_val_datasets,
+                                                                                  index_features_list,
+                                                                                  index_names_list,
+                                                                                  idx_to_dress_mapping):
+                    recall_at10, recall_at50 = compute_fiq_val_metrics(relative_val_dataset, clip_model, index_features,
+                                                                       index_names, combiner.combine_features)
+                    recalls_at10.append(recall_at10)
+                    recalls_at50.append(recall_at50)
+
+                results_dict = {}
+                for i in range(len(recalls_at10)):
+                    results_dict[f'{idx_to_dress_mapping[i]}_recall_at10'] = recalls_at10[i]
+                    results_dict[f'{idx_to_dress_mapping[i]}_recall_at50'] = recalls_at50[i]
+                results_dict.update({
+                    f'average_recall_at10': mean(recalls_at10),
+                    f'average_recall_at50': mean(recalls_at50),
+                    f'average_recall': (mean(recalls_at50) + mean(recalls_at10)) / 2
+                })
+
+                print(json.dumps(results_dict, indent=4))
+                experiment.log_metrics(
+                    results_dict,
+                    epoch=epoch
+                )
+
+                # Validation CSV logging
+                log_dict = {'epoch': epoch}
+                log_dict.update(results_dict)
+                validation_log_frame = pd.concat([validation_log_frame, pd.DataFrame(data=log_dict, index=[0])])
+                validation_log_frame.to_csv(str(training_path / 'validation_metrics.csv'), index=False)
+
+            # Save model
+            if save_training:
+                if save_best and results_dict['average_recall'] > best_avg_recall:
+                    best_avg_recall = results_dict['average_recall']
+                    save_model('combiner', epoch, combiner, training_path)
+                elif not save_best:
+                    save_model(f'combiner_{epoch}', epoch, combiner, training_path)
+
+
 def combiner_training_cirr(projection_dim: int, hidden_dim: int, num_epochs: int, clip_model_name: str,
                            combiner_lr: float, batch_size: int, clip_bs: int, validation_frequency: int, transform: str,
                            save_training: bool, save_best: bool, **kwargs):
@@ -421,7 +627,7 @@ def combiner_training_cirr(projection_dim: int, hidden_dim: int, num_epochs: int
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument("--dataset", type=str, required=True, help="should be either 'CIRR' or 'fashionIQ'")
+    parser.add_argument("--dataset", type=str, required=True, help="should be either 'CIRR' or 'fashionIQ' or 'adidas'")
     parser.add_argument("--api-key", type=str, help="api for Comet logging")
     parser.add_argument("--workspace", type=str, help="workspace of Comet logging")
     parser.add_argument("--experiment-name", type=str, help="name of the experiment on Comet")
@@ -443,8 +649,8 @@ if __name__ == '__main__':
                         help="Save only the best model during training")
 
     args = parser.parse_args()
-    if args.dataset.lower() not in ['fashioniq', 'cirr']:
-        raise ValueError("Dataset should be either 'CIRR' or 'FashionIQ")
+    if args.dataset.lower() not in ['fashioniq', 'cirr', 'adidas']:
+        raise ValueError("Dataset should be either 'CIRR' or 'FashionIQ' or 'adidas'")
 
     training_hyper_params = {
         "projection_dim": args.projection_dim,
@@ -490,3 +696,7 @@ if __name__ == '__main__':
         training_hyper_params.update(
             {'train_dress_types': ['dress', 'toptee', 'shirt'], 'val_dress_types': ['dress', 'toptee', 'shirt']})
         combiner_training_fiq(**training_hyper_params)
+    elif args.dataset.lower() == 'adidas':
+        training_hyper_params.update(
+            {'train_dress_types': ['general'], 'val_dress_types': ['general']})
+        combiner_training_adidas(**training_hyper_params)
--- src/validate.py
+++ src/validate.py
@@ -132,6 +132,120 @@ def fashioniq_val_retrieval(dress_type: str, combining_function: callable, clip_
                                    combining_function)
 
 
+def compute_adidas_val_metrics(relative_val_dataset: FashionIQDataset, clip_model: CLIP, index_features: torch.tensor,
+                            index_names: List[str], combining_function: callable) -> Tuple[float, float]:
+    """
+    Compute validation metrics on Adidas dataset
+    :param relative_val_dataset: Adidas validation dataset in relative mode
+    :param clip_model: CLIP model
+    :param index_features: validation index features
+    :param index_names: validation index names
+    :param combining_function: function which takes as input (image_features, text_features) and outputs the combined
+                            features
+    :return: the computed validation metrics
+    """
+
+    # Generate predictions
+    predicted_features, target_names = generate_adidas_val_predictions(clip_model, relative_val_dataset,
+                                                                    combining_function, index_names, index_features)
+
+    print(f"Compute Adidas {relative_val_dataset.dress_types} validation metrics")
+
+    # Normalize the index features
+    index_features = F.normalize(index_features, dim=-1).float()
+
+    # Compute the distances and sort the results
+    distances = 1 - predicted_features @ index_features.T
+    sorted_indices = torch.argsort(distances, dim=-1).cpu()
+    sorted_index_names = np.array(index_names)[sorted_indices]
+
+    # Compute the ground-truth labels wrt the predictions
+    labels = torch.tensor(
+        sorted_index_names == np.repeat(np.array(target_names), len(index_names)).reshape(len(target_names), -1))
+    assert torch.equal(torch.sum(labels, dim=-1).int(), torch.ones(len(target_names)).int())
+
+    # Compute the metrics
+    recall_at10 = (torch.sum(labels[:, :10]) / len(labels)).item() * 100
+    recall_at50 = (torch.sum(labels[:, :50]) / len(labels)).item() * 100
+
+    return recall_at10, recall_at50
+
+
+def generate_adidas_val_predictions(clip_model: CLIP, relative_val_dataset: FashionIQDataset,
+                                 combining_function: callable, index_names: List[str], index_features: torch.tensor) -> \
+        Tuple[torch.tensor, List[str]]:
+    """
+    Compute Adidas predictions on the validation set
+    :param clip_model: CLIP model
+    :param relative_val_dataset: Adidas validation dataset in relative mode
+    :param combining_function: function which takes as input (image_features, text_features) and outputs the combined
+                            features
+    :param index_features: validation index features
+    :param index_names: validation index names
+    :return: predicted features and target names
+    """
+    print(f"Compute Adidas {relative_val_dataset.dress_types} validation predictions")
+
+    relative_val_loader = DataLoader(dataset=relative_val_dataset, batch_size=32,
+                                     num_workers=multiprocessing.cpu_count(), pin_memory=True, collate_fn=collate_fn,
+                                     shuffle=False)
+
+    # Get a mapping from index names to index features
+    name_to_feat = dict(zip(index_names, index_features))
+
+    # Initialize predicted features and target names
+    predicted_features = torch.empty((0, clip_model.visual.output_dim)).to(device, non_blocking=True)
+    target_names = []
+
+    for reference_names, batch_target_names, captions in tqdm(relative_val_loader):  # Load data
+
+        # Concatenate the captions in a deterministic way
+        flattened_captions: list = np.array(captions).T.flatten().tolist()
+        input_captions = [
+            f"{flattened_captions[i].strip('.?, ').capitalize()} and {flattened_captions[i + 1].strip('.?, ')}" for
+            i in range(0, len(flattened_captions), 2)]
+        text_inputs = clip.tokenize(input_captions, context_length=77).to(device, non_blocking=True)
+
+        # Compute the predicted features
+        with torch.no_grad():
+            text_features = clip_model.encode_text(text_inputs)
+            # Check whether a single element is in the batch due to the exception raised by torch.stack when used with
+            # a single tensor
+            if text_features.shape[0] == 1:
+                reference_image_features = itemgetter(*reference_names)(name_to_feat).unsqueeze(0)
+            else:
+                reference_image_features = torch.stack(itemgetter(*reference_names)(
+                    name_to_feat))  # To avoid unnecessary computation retrieve the reference image features directly from the index features
+            batch_predicted_features = combining_function(reference_image_features, text_features)
+
+        predicted_features = torch.vstack((predicted_features, F.normalize(batch_predicted_features, dim=-1)))
+        target_names.extend(batch_target_names)
+
+    return predicted_features, target_names
+
+
+def adidas_val_retrieval(dress_type: str, combining_function: callable, clip_model: CLIP, preprocess: callable):
+    """
+    Perform retrieval on Adidas validation set computing the metrics. To combine the features the `combining_function`
+    is used
+    :param dress_type: Adidas category on which perform the retrieval
+    :param combining_function:function which takes as input (image_features, text_features) and outputs the combined
+                            features
+    :param clip_model: CLIP model
+    :param preprocess: preprocess pipeline
+    """
+
+    clip_model = clip_model.float().eval()
+
+    # Define the validation datasets and extract the index features
+    classic_val_dataset = FashionIQDataset('val', [dress_type], 'classic', preprocess)
+    index_features, index_names = extract_index_features(classic_val_dataset, clip_model)
+    relative_val_dataset = FashionIQDataset('val', [dress_type], 'relative', preprocess)
+
+    return compute_adidas_val_metrics(relative_val_dataset, clip_model, index_features, index_names,
+                                   combining_function)
+
+
 def compute_cirr_val_metrics(relative_val_dataset: CIRRDataset, clip_model: CLIP, index_features: torch.tensor,
                              index_names: List[str], combining_function: callable) -> Tuple[
     float, float, float, float, float, float, float]:
@@ -262,7 +376,7 @@ def cirr_val_retrieval(combining_function: callable, clip_model: CLIP, preproces
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--dataset", type=str, required=True, help="should be either 'CIRR' or 'fashionIQ'")
+    parser.add_argument("--dataset", type=str, required=True, help="should be either 'CIRR' or 'fashionIQ' or 'adidas'")
     parser.add_argument("--combining-function", type=str, required=True,
                         help="Which combining function use, should be in ['combiner', 'sum']")
     parser.add_argument("--combiner-path", type=Path, help="path to trained Combiner")
@@ -352,8 +466,23 @@ def main():
 
         print(f"average recall10 = {mean(average_recall10_list)}")
         print(f"average recall50 = {mean(average_recall50_list)}")
+    elif args.dataset.lower() == 'adidas':
+        average_recall10_list = []
+        average_recall50_list = []
+
+        general_recallat10, general_recallat50 = adidas_val_retrieval('general', combining_function, clip_model,
+                                                                     preprocess)
+        average_recall10_list.append(general_recallat10)
+        average_recall50_list.append(general_recallat50)
+
+        print(f"\n{general_recallat10 = }")
+        print(f"{general_recallat50 = }")
+
+        print(f"average recall10 = {mean(average_recall10_list)}")
+        print(f"average recall50 = {mean(average_recall50_list)}")
+
     else:
-        raise ValueError("Dataset should be either 'CIRR' or 'FashionIQ")
+        raise ValueError("Dataset should be either 'CIRR' or 'FashionIQ' or 'adidas'")
 
 
 if __name__ == '__main__':
